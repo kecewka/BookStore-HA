185894509759b5b6fa280a65af19fefbb38d24ac
kecewka <a_aytkazin@kbtu.kz>
2022-07-27 17:49:28 +0600

    Sprint 2 update repo

--- src/main/java/kz/halykacademy/bookstore/dao/AuthorRepository.java
+++ src/main/java/kz/halykacademy/bookstore/dao/AuthorRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
     public List<Author> findAllByNameContaining(String name);
+
 }
--- src/main/java/kz/halykacademy/bookstore/dao/BookRepository.java
+++ src/main/java/kz/halykacademy/bookstore/dao/BookRepository.java
@@ -1,10 +1,13 @@
 package kz.halykacademy.bookstore.dao;
 
 import kz.halykacademy.bookstore.entity.Book;
+import kz.halykacademy.bookstore.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
     public List<Book> findAllByTitleContaining(String titlepart);
+
+    public List<Book> findAllByGenresList(List<Genre> genres);
 }
